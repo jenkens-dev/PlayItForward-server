715ccb1ad033abff56eed7edb992bf79ef0d1f34
Taljjaa <jenken131@gmail.com>
2020-04-21 19:31:43 -0700

    setup sequelize and added models

---
+++ src/models/event.js
@@ -0,0 +1,15 @@
+module.exports = (sequelize, DataTypes) => {
+  const Event = sequelize.define('event', {
+    title: DataTypes.STRING,
+    date: DataTypes.STRING,
+    address: DataTypes.STRING,
+  });
+
+  Event.associate = (models) => {
+    Event.belongsTo(models.nonprofit, {
+      foreignKey: 'nonprofitId',
+    });
+  };
+
+  return Event;
+};
---
+++ src/models/index.js
@@ -0,0 +1,22 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('pif', 'postgres', 'postgres', {
+  dialect: 'postgres',
+});
+
+const models = {
+  event: sequelize.import('./event'),
+  nonprofit: sequelize.import('./nonprofit'),
+  volunteer: sequelize.import('./volunteer'),
+};
+
+Object.keys(models).forEach((modelName) => {
+  if ('associate' in models[modelName]) {
+    models[modelName].associate(models);
+  }
+});
+
+models.sequelize = sequelize;
+models.Sequelize = Sequelize;
+
+module.exports = models;
