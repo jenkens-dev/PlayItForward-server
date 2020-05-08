31d95c163ecf8a47dc2cc63756ff3b8f64fd8ef4
Taljjaa <jenken131@gmail.com>
2020-05-07 20:57:24 -0700

    server working with some ts

--- src/resolvers/singleUpload.js
+++ src/resolvers/singleUpload.js
@@ -1,18 +1,3 @@
-// import { AWSS3Uploader } from '../lib/uploaders/s3.ts';
-
-// const s3Uploader = new AWSS3Uploader({
-//   accessKeyId: process.env.AWS_ACCESS_KEY,
-//   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-//   destinationBucketName: 'test-graphql-uploads',
-// });
-
-// const resolvers = {
-//   Mutation: {
-//     singleUpload: s3Uploader.singleFileUploadResolver.bind(s3Uploader),
-//   },
-// };
-
-
 export default {
   Mutation: {
     singleUpload: async (parent, { file }) => {
