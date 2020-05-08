1aa7feb9dd99108d5bf4b80e71d28d49b84a4dfc
Taljjaa <jenken131@gmail.com>
2020-05-07 19:30:58 -0700

    added single upload mutation

--- src/resolvers/singleUpload.js
+++ src/resolvers/singleUpload.js
@@ -12,4 +12,15 @@
 //   },
 // };
 
-// module.exports = resolvers;
+
+export default {
+  Mutation: {
+    singleUpload: async (parent, { file }) => {
+      const { stream, filename, mimetype, encoding } = await file;
+
+      // Do work 💪
+
+      return { filename, mimetype, encoding, url: '' };
+    },
+  },
+};
