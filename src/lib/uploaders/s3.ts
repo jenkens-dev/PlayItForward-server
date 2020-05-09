75746a2eb217645e09674155fca9782697fe3bb9
Taljjaa <jenken131@gmail.com>
2020-05-09 12:00:45 -0700

    added tsnode package and converting files to ts

--- src/lib/uploaders/s3.ts
+++ src/lib/uploaders/s3.ts
@@ -1,4 +1,5 @@
 import AWS from 'aws-sdk';
+import { ApolloServerFileUploads } from '../index';
 
 type S3UploadConfig = {
   accessKeyId: string;
@@ -7,19 +8,27 @@ type S3UploadConfig = {
   region?: string;
 };
 
-export class AWSS3Uploader {
+export class AWSS3Uploader implements ApolloServerFileUploads.IUploader {
   private s3: AWS.S3;
   public config: S3UploadConfig;
 
   constructor(config: S3UploadConfig) {
     AWS.config = new AWS.Config();
     AWS.config.update({
-      region: config.region || 'ca-central-1',
+      region: config.region || 'us-west-2',
       accessKeyId: config.accessKeyId,
       secretAccessKey: config.secretAccessKey,
     });
 
     this.s3 = new AWS.S3();
     this.config = config;
+  }
+
+  async singleFileUploadResolver(
+    parent,
+    { file }: { file: Promise<ApolloServerFileUploads.File> },
+  ): Promise<ApolloServerFileUploads.UploadedFileResponse> {
+    // Todo next!
+    return null;
   }
 }
