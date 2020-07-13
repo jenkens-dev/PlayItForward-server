5a5070e8335564382d50396bdeafc903c2dcabe8
Taljjaa <jenken131@gmail.com>
2020-07-13 11:47:13 -0700

    changed variable name from image to file

--- src/schema/event.ts
+++ src/schema/event.ts
@@ -24,7 +24,7 @@ export default gql`
   type Mutation {
     createEvent(
       title: String!
-      image: Upload
+      file: Upload
       date: String!
       location: String!
       nonprofitId: Int!
